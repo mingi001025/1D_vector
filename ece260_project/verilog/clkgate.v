bbd8ade9204a2133f96672f5490deab1a7dd1ccd
seanlipps <slipps@ucsd.edu>
2025-03-22 01:49:21 -0700

    pipelining (from mingi)

---
+++ ece260_project/verilog/clkgate.v
@@ -0,0 +1,15 @@
+module clkgate(en, clk, gclk);
+     input   clk;
+     input   en;
+     output  gclk;
+ 
+     reg     en_q;
+ 
+     assign gclk = en_q & en;
+     
+     always @(!clk) begin
+         en_q <= en;
+     end
+ 
+ 
+ endmodule
