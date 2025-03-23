355b8479f3487714dba19fc9056524ff07119b67
Venkateshwaran Sivaramakrishnan <vsivaramakrishnan@ucsd.edu>
2025-03-22 22:08:48 -0700

    Step3: part deliverables

---
+++ deliverables/step3/gate_sim/xcelium.d/run.lnx8664.20.09.d/.timestamp.ts
@@ -0,0 +1,3 @@
+1737007846 /home/linux/ieng6/ee260bwi25/public/PDKdata/verilog/tcbn65gplus.v
+1742696554 /home/linux/ieng6/ee260bwi25/vsivaramakrishnan/delete/1D_vector/deliverables/step3/gate_sim/netlist/verilog/core.pnr.v
+1742698645 /home/linux/ieng6/ee260bwi25/vsivaramakrishnan/delete/1D_vector/deliverables/step3/gate_sim/netlist/verilog/fullchip_tb.v
